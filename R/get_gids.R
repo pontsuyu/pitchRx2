9653d8a44d10f2dc1a414306bec3162d9b775ccd
tsuyuzaki <tsuyuzaki@macromill.com>
2017-12-27 20:12:19 +0900

    update script

---
+++ R/get_gids.R
@@ -0,0 +1,35 @@
+#' Title
+#'
+#' @param years
+#'
+#' @return
+#' @import tidyverse
+#' @import rvest
+#'
+#' @export
+#'
+#' @examples
+get_gids <- function(start_year = 2016, end_year = 2017){
+  if(start_year<2010||end_year>lubridate::year(Sys.Date()))
+    stop("'years' must be between 2015 and 2017.")
+
+  date <- data.frame(date = seq(as.Date(paste0(start_year, "-01-01")), as.Date(paste0(end_year, "-12-31")), by = "day"), stringsAsFactors = F) %>%
+          separate(date, c("year", "month", "day"), sep = "-")
+  candidate <- paste0(
+      "http://gd2.mlb.com/components/game/mlb/year_", year = date[["year"]],
+      "/month_", month = date[["month"]],
+      "/day_", day = date[["day"]]
+      )
+  res_list <- candidate %>% map(function(x) {
+    tmp <- try(read_html(x), silent = T)
+    if (all(class(tmp) != "try-error")) {
+      tmp %>%
+        html_nodes("li") %>%
+        html_text() %>%
+        str_subset("^ gid_") %>%
+        str_subset(".*mlb.*") %>%
+        str_replace_all(" |/", "")
+    }
+  })
+  return(do.call("c", res_list))
+}
--- sff.R
+++ sff.R
@@ -3,19 +3,20 @@ library(tidyverse)
 library(DBI)
 db <- src_sqlite("sff.sqlite3", create = T)
 # files <- c("miniscoreboard.xml", "players.xml", "inning/inning_hit.xml")
+nya <- game_ids %>% str_subset("2017.*nya.*")
 system.time({
-  scrape(start = "2017-06-01", end = "2017-06-30", connect = db$con)
+  scrape(game.ids = nya, connect = db$con)
 })
 db_list_tables(db$con)
-# [1] "action" "atbat"  "coach"  "game"   "hip"    "media"  "pitch"
-# [8] "player" "po"     "runner" "umpire"
+# [1] "action" "atbat" "pitch" "po" "runner"
 pitch <- dbGetQuery(db$con, "SELECT * FROM pitch")
 atbat <- dbGetQuery(db$con, "SELECT * FROM atbat")
 notneed <- c("code", "event_num", "play_guid", "end_tfs_zulu",
              "event_num", "event_es", "play_guid", "event2_es")
 atbat <- atbat %>% select(-(29:33))
-unique(atbat$pitcher_name)
-dat <- inner_join(pitch, atbat) %>% filter(pitcher_name=="Masahiro Tanaka")
+sort(unique(pitch$pitcher_name))
+sort(unique(atbat$pitcher_name))
+dat <- inner_join(pitch, atbat) %>% filter(pitcher_name=="Adam Warren")
 x <- list(
   facet_grid(pitcher_name ~ stand, labeller = label_both),
   theme_bw(),
