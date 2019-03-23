956c0f28e1e745c062d5c04012871c80867f08c7
hiroyuki <wu.pe.s2.47@gmail.com>
2019-03-23 11:34:12 +0900

    update

--- R/scrape_inning_all.R
+++ R/scrape_inning_all.R
@@ -10,7 +10,8 @@
 #' @importFrom xml2 read_html
 #' @importFrom XML xmlParse
 #' @importFrom stringr str_detect
-#' @importFrom purrr map
+#' @import foreach
+#' @import doParallel
 #' @import XML2R
 #' @import dplyr
 #'
@@ -22,17 +23,13 @@
 #'
 scrape_inning_all <- function(gids, db_name = "database") {
     URLs <- paste0(makeUrls(gids), "/inning/inning_all.xml")
-    docs <- map(URLs, function(URL){
-        text <- try(read_html(URL), silent = T)
-        if(class(text)[1] != "try-error"){
-            xmlParse(text, asText = TRUE)
-        }
-    })
+    cores <- detectCores(logical=FALSE)
+    cluster <- makeCluster(cores)
+    registerDoParallel(cluster)
+    obs <- foreach(i = 1:length(URLs), .combine = c, .packages = "XML2R") %dopar%
+        XML2Obs(URLs[i], quiet = TRUE)
+    stopCluster(cluster)
     suppressWarnings({
-        obs <- docsToNodes(docs, "/") %>%
-            nodesToList() %>%
-            listsToObs(urls = URLs, url.map = FALSE)
-        obs <- XML2Obs(URLs, quiet = TRUE)
         obs <- re_name(obs, equiv = c("game//inning//top//atbat//pitch", "game//inning//bottom//atbat//pitch"),
                        diff.name = "inning_side", quiet = TRUE)
         obs <- re_name(obs, equiv = c("game//inning//top//atbat//runner", "game//inning//bottom//atbat//runner"),
@@ -64,7 +61,7 @@ scrape_inning_all <- function(gids, db_name = "database") {
     tab.nms <- gsub(".*runner$", "runner", tab.nms)
     tab.nms <- gsub(".*pitch$", "pitch", tab.nms)
     tables <- setNames(tables, tab.nms)
-    tables <- lapply(tables, function(x) as.data.frame(x=x, stringsAsFactors=F))
+    tables <- lapply(tables, function(x) as.data.frame(x = x, stringsAsFactors = FALSE))
 
     colnames(tables$atbat) <- sub("^des", "atbat_des", colnames(tables$atbat))
     for(i in names(tables)) tables[[i]] <- format.table(tables[[i]], name=i)
@@ -72,9 +69,9 @@ scrape_inning_all <- function(gids, db_name = "database") {
     tables$pitch <- appendPitchCount(tables$pitch)
     tables$atbat <- appendDate(tables$atbat)
 
+    # make data-base file
     fn <- paste0(db_name, ".sqlite3")
     if(file.exists(fn)) file.remove(fn)
-    # make data-base file
     db <- src_sqlite(fn, create = T)
 
     for (i in names(tables)){
@@ -92,14 +89,6 @@ scrape_inning_all <- function(gids, db_name = "database") {
 # Take a matrix and turn into data frame and turn relevant columns into numerics
 format.table <- function(dat, name) {
     switch(name,
-           game = nums <- c("venue_id", "scheduled_innings", "away_team_id", "away_league_id", "home_team_id",
-                            "home_league_id", "away_games_back_wildcard", "away_win",  "away_loss", "home_win",
-                            "home_loss", "inning", "outs", "away_team_runs","home_team_runs", "away_team_hits",
-                            "home_team_hits", "away_team_errors", "home_team_errors"),
-           player = nums <- c("id", "num", "avg", "hr", "rbi", "bat_order", "wins", "losses", "era"),
-           coach = nums <- c("id", "num"),
-           umpire = nums <- "id",
-           hip = nums <- c("x", "y", "batter", "pitcher", "inning"),
            action = nums <- c("b", "s", "o", "player", "pitch", "inning"),
            atbat = nums <- c("pitcher", "batter", "num", "b", "s", "o", "inning"),
            pitch = nums <- c("id", "x", "y", "start_speed", "end_speed", "sz_top", "sz_bot", "pfx_x", "pfx_z", "px",
@@ -111,14 +100,7 @@ format.table <- function(dat, name) {
     dat <- unique(dat)
     numz <- nums[nums %in% names(dat)] #error handling (just in case one of the columns doesn't exist)
     for (i in numz) dat[, i] <- suppressWarnings(as.numeric(dat[, i]))
-    if ("game" %in% name) {
-        dat$url_scoreboard <- dat$url
-        dat$url <- paste0(gsub("miniscoreboard.xml", "", dat$url), "gid_", dat$gameday_link, "/inning/inning_all.xml")
-        # These fields only show up for suspended games...I don't think they're worth tracking...
-        dat <- dat[, !names(dat) %in% c("runner_on_base_status", "runner_on_1b")]
-    } else { #create a 'gameday_link' column for easier linking of tables
-        if (length(grep("^url$", names(dat)))) dat$gameday_link <- sub("/.*", "", sub(".*gid", "gid", dat$url))
-    }
+    if (length(grep("^url$", names(dat)))) dat$gameday_link <- sub("/.*", "", sub(".*gid", "gid", dat$url))
     return(dat)
 }
 
